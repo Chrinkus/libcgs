f703aa51ad5f8684b2dbad27acd0ede07567be1b
Chris Schick <seeschickrun@gmail.com>
2022-04-12 22:42:29 -0500

    Changed the io tests over to cmocka
    - Had fun using the setup and teardown to create my test data. Can
      probably remove the /data folder from root.

--- test/tests_io.c
+++ test/tests_io.c
@@ -1,111 +1,135 @@
+#include "cmocka_headers.h"
+
 #include "cgs_io.h"
-#include "cgs_test.h"
 
-/* io_test_data.txt contents:
- * Birthday
- * Christmas
- * Tuesday
- * Books for reading
- *
- */
+#include <stdlib.h>	// free
 
-int io_getline_test(void* data)
+const char* const data_path = "io_test_data.txt";
+
+static int setup_file_content(void** state)
 {
-	const char* path = (const char*)data;
+	(void)state;
 
-	FILE* file = fopen(path, "r");
+	FILE* file = fopen(data_path, "w");
+
+	if (!file)
+		return -1;
+
+	fputs("Birthday\n", file);
+	fputs("Christmas\n", file);
+	fputs("Tuesday\n", file);
+	fputs("Books for reading\n", file);
+
+	fclose(file);
+
+	return 0;
+}
+
+static int setup_file_read(void** state)
+{
+	FILE* file = fopen(data_path, "r");
+
+	if (!file)
+		return -1;
+
+	*state = file;
+
+	return 0;
+}
+
+static int teardown_file_read(void** state)
+{
+	FILE* file = *(FILE**)state;
+	fclose(file);
+	return 0;
+}
+
+static void io_getline_test(void** state)
+{
+	FILE* file = *(FILE**)state;
 
 	struct cgs_string* buff = cgs_string_new();
 	int n = cgs_io_getline(file, buff);
 
-	assert(n == 8);
-	assert(strcmp(cgs_string_read(buff), "Birthday") == 0);
+	assert_int_equal(n, 8);
+	assert_string_equal(cgs_string_read(buff), "Birthday");
 
 	cgs_string_clear(buff);
 	n = cgs_io_getline(file, buff);
-	assert(n == 9);
-	assert(strcmp(cgs_string_read(buff), "Christmas") == 0);
+	assert_int_equal(n, 9);
+	assert_string_equal(cgs_string_read(buff), "Christmas");
 
 	cgs_string_clear(buff);
 	n = cgs_io_getline(file, buff);
-	assert(n == 7);
-	assert(strcmp(cgs_string_read(buff), "Tuesday") == 0);
+	assert_int_equal(n, 7);
+	assert_string_equal(cgs_string_read(buff), "Tuesday");
 
 	cgs_string_clear(buff);
 	n = cgs_io_getline(file, buff);
-	assert(n == 17);
-	assert(strcmp(cgs_string_read(buff), "Books for reading") == 0);
+	assert_int_equal(n, 17);
+	assert_string_equal(cgs_string_read(buff), "Books for reading");
 
 	cgs_string_clear(buff);
 	n = cgs_io_getline(file, buff);
-	assert(n == 0);
-	assert(strcmp(cgs_string_read(buff), "") == 0);
+	assert_int_equal(n, 0);
+	assert_string_equal(cgs_string_read(buff), "");
 
 	cgs_string_free(buff);
-	fclose(file);
-
-	return TEST_SUCCESS;
 }
 
-int io_readline_test(void* data)
+static void io_readline_test(void** state)
 {
-	const char* path = (const char*)data;
-	FILE* file = fopen(path, "r");
+	FILE* file = *(FILE**)state;
 
 	char* line = cgs_io_readline(file);
-	assert(strcmp(line, "Birthday") == 0);
+	assert_string_equal(line, "Birthday");
 	free(line);
 
 	line = cgs_io_readline(file);
-	assert(strcmp(line, "Christmas") == 0);
+	assert_string_equal(line, "Christmas");
 	free(line);
 	line = cgs_io_readline(file);
-	assert(strcmp(line, "Tuesday") == 0);
+	assert_string_equal(line, "Tuesday");
 	free(line);
 	line = cgs_io_readline(file);
-	assert(strcmp(line, "Books for reading") == 0);
+	assert_string_equal(line, "Books for reading");
 	free(line);
 
 	line = cgs_io_readline(file);
-	assert(line == NULL);
-
-	fclose(file);
-
-	return TEST_SUCCESS;
+	assert_null(line);
 }
 
-int io_readlines_test(void* data)
+static void io_readlines_test(void** state)
 {
-	const char* path = (const char*)data;
-	FILE* file = fopen(path, "r");
+	FILE* file = *(FILE**)state;
 
 	struct cgs_array* lines = cgs_io_readlines(file);
-	assert(cgs_array_length(lines) == 4);
+	assert_int_equal(cgs_array_length(lines), 4);
 
 	CgsStrIter b = cgs_array_begin(lines);
 	CgsStrIter e = cgs_array_end(lines);
-	assert(b != e);
+	assert_ptr_not_equal(b, e);
 
-	assert(strcmp(*b++, "Birthday") == 0);
-	assert(strcmp(*b++, "Christmas") == 0);
-	assert(strcmp(*b++, "Tuesday") == 0);
-	assert(strcmp(*b++, "Books for reading") == 0);
-	assert(b == e);
+	assert_string_equal(*b++, "Birthday");
+	assert_string_equal(*b++, "Christmas");
+	assert_string_equal(*b++, "Tuesday");
+	assert_string_equal(*b++, "Books for reading");
+	assert_ptr_equal(b, e);
 
 	cgs_array_free_all(lines);
-	fclose(file);
-
-	return TEST_SUCCESS;
 }
 
 int main(void)
 {
-	struct test tests[] = {
-		{ "io_getline", io_getline_test, "io_test_data.txt" },
-		{ "io_readline", io_readline_test, "io_test_data.txt" },
-		{ "io_readlines", io_readlines_test, "io_test_data.txt" },
+	const struct CMUnitTest tests[] = {
+		cmocka_unit_test_setup_teardown(io_getline_test,
+				setup_file_read, teardown_file_read),
+		cmocka_unit_test_setup_teardown(io_readline_test,
+				setup_file_read, teardown_file_read),
+		cmocka_unit_test_setup_teardown(io_readlines_test,
+				setup_file_read, teardown_file_read),
 	};
 
-	return cgs_run_tests(tests);
+	return cmocka_run_group_tests(tests, setup_file_content, NULL);
 }
 
